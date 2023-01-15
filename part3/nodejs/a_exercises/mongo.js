04ac43a9020fb81886b16236c11f55362f2cc050
toto <tobirastorza@gmail.com>
2023-01-15 19:28:09 -0300

    15 jan: continue testing, should fix folders

--- part3/nodejs/a_exercises/mongo.js
+++ part3/nodejs/a_exercises/mongo.js
@@ -9,7 +9,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://tovillero:${password}@fso.gobtmma.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://tovillero:${password}@fso.gobtmma.mongodb.net/?retryWrites=true&w=majority`;
 
 const noteSchema = new mongoose.Schema(
   {
@@ -33,13 +33,13 @@ mongoose
   .then((result) => {
     console.log("connected");
 
-    // const note = new Note({
-    // content: 'New note',
-    // date: new Date(),
-    // important: true,
-    // })
+    const note = new Note({
+    content: 'New note',
+    date: new Date(),
+    important: true,
+    })
 
-    // return note.save()
+    return note.save()
 
     Note.findByImportant()
       .exec((err, notes) => {
@@ -48,7 +48,7 @@ mongoose
       })
   })
   .then(() => {
-    // console.log('note saved!')
-    // mongoose.connection.close()
+    console.log('note saved!')
+    mongoose.connection.close()
   })
   .catch((err) => console.log(err));
