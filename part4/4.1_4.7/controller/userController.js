3fc67f039f1546e5bc38f82535d7b959bbe7bf6b
tobias <tobirastorza@gmail.com>
2023-04-30 22:35:21 -0300

    4.16 finished

--- part4/4.1_4.7/controller/userController.js
+++ part4/4.1_4.7/controller/userController.js
@@ -1,12 +1,17 @@
 const userRouter = require('express').Router();
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
+const { valueGreaterThanThree } = require('../utils/helpers');
 
 userRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body;
 
-    if(!username || !name || !password) {
-        return response.status(400).json({ error: 'username, name or password is missing' });
+    if(!valueGreaterThanThree(username)) {
+        return response.status(400).json({ error: 'username must be larger than 3' });
+    }
+
+    if(!valueGreaterThanThree(password)) {
+        return response.status(400).json({ error: 'password must be larger than 3' });
     }
 
     const saltRounds = 10;
